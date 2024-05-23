4ed1c4d037a9e52d86233b2d4cc312c118086ea9
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-23 12:22:57 +0530

    using ternary operator-conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 const App = () => {
 
   const isLoggedIn = true;
-
-  if (isLoggedIn) {
-    return <h1>Welcome Back!</h1>
-  } else {
-    return <h1>Please Log In</h1>
+return (
+  <div>
+  {
+  isLoggedIn ? <h1>Welcome Back!</h1> : <h1>Please Log In</h1>
   }
+  </div>
+)
 }
-
 export default App
