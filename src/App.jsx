12c87a4b21324c5bd0a97bb6aeb2794f1d5b2f6e
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-24 12:04:02 +0530

    functional component and class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
-const App = () => {
-  return <h1>Hello, World!</h1>
+
+// // Functional component with arrow function 
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>My React App</h1>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// Functional component with function 
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>My React App</h1>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+// class component 
+
+import { Component } from "react";
+
+class App extends Component {
+  // override the render method
+  render() {
+    //return JSX
+    return (
+      <div>
+        <h1>My React App</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
