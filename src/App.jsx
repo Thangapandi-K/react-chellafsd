b036d84725084914c598308478d52e1c2dad0ed4
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-24 09:33:18 +0530

    todo moved to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import Todo from "./components/Todo"
+
+
 const App = () => {
 
-  const isLoggedIn = false;
-return (
-  <div>
-  {isLoggedIn && <h1>Welcome Back!</h1>} 
-  {!isLoggedIn && <h1>Please Log In</h1>}
-  </div>
-)
+const todos = [
+  {id:1, title: 'Buy Milk', completed: false},
+  {id:2, title: 'Buy Bread', completed: true},
+  {id:3, title: 'Buy Butter', completed: false}
+]
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {
+          todos.map(todo => 
+            <Todo 
+              key = {todo.id}
+              todo = {todo}
+            />
+          )
+        }
+      </ul>
+    </div>
+  )
 }
+
 export default App
