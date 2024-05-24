746696f80b47b8f636c435e9a3786bbc0b4f6944
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-24 09:50:52 +0530

    event handling ---onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import Todo from "./components/Todo"
 
 
 const App = () => {
 
-const todos = [
-  {id:1, title: 'Buy Milk', completed: false},
-  {id:2, title: 'Buy Bread', completed: true},
-  {id:3, title: 'Buy Butter', completed: false}
-]
+const handleClick = () => {
+  console.log('Button Clicked!')
+}
+
 
   return (
     <div>
-      <h1>Todo List</h1>
-      <ul>
-        {
-          todos.map(todo => 
-            <Todo 
-              key = {todo.id}
-              todo = {todo}
-            />
-          )
-        }
-      </ul>
+      <h1>React App</h1>
+      <button onClick={handleClick}>Click Me!</button>
     </div>
   )
+
+
+
+
 }
 
-export default App
+export default App;
