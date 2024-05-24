f56584e5cb427858a2e7796d1699194bacd71d0e
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-24 10:37:22 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-
+import React from 'react'
 
 const App = () => {
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-  }
-  
   return (
-    <div>
-      <h1>React App</h1>
-      <input 
-        type="text"
-        onChange={handleChange} 
-      />
-    </div>
+    <div>App</div>
   )
-
-
 }
 
-export default App;
+export default App
