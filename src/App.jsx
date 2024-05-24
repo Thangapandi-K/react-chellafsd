c4261eb9677e092d043191b3673b8d6b54b95666
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-24 12:13:09 +0530

    adding multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 
-// // Functional component with arrow function 
-
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>My React App</h1>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-// Functional component with function 
-
-// function App() {
-//   return (
-//     <div>
-//       <h1>My React App</h1>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-
 // class component 
 
 import { Component } from "react";
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   // override the render method
@@ -34,7 +19,7 @@ class App extends Component {
     //return JSX
     return (
       <div>
-        <h1>My React App</h1>
+        <Hello />
       </div>
     )
   }
