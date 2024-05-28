f52a435ad8581116c6a008ad052b89608bcf52d9
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-28 09:02:43 +0530

    useEffect -------with dependency list

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import { useState } from 'react'
 const App = () => {
 
   let [count, setCount] = useState(0);
+  let [increment, setIncrement] = useState(0);
 
   //runs when the component is mounted and count is updated
   useEffect(() => {
     console.log('component mounted');
+    console.log('Count', count);
+    console.log('Increment', increment);
   }, [count]);
 
   const handleIncrement = () => {
     setCount(count + 1);
+    setIncrement(increment + 1);
   }
 
   const handleDecrement = () => {
