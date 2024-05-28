b75a9bea9cc3d367867f73c0616666c84ac84ca7
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-28 12:40:11 +0530

    basic react route dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World!!</h1>
+  },
+  {
+    path: "/about",
+    element: <h1>About Us</h1>
+  },
+  {
+    path: "/contact",
+    element: <h1>Contact Us</h1>
+  },
+  {
+    path: "/services",
+    element: <h1>Our Services</h1>
+  },
+  {
+    path: "/products",
+    element: <h1>Our Products</h1>
+  }
+]);
+
 const App = () => {
-  return <h1>Hello, World!</h1>
+  return <RouterProvider router={router} />
 }
 
 export default App;
