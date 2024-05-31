cdf4c67f3495bdf48353f23bf7955f16a80f18fd
Thangapandi-K <thangapandi.chella@gmail.com>
2024-05-31 11:22:26 +0530

    status change in the context

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { useContext } from "react";
+import { useEffect } from "react";
+import { createContext } from "react"
+import { useState } from "react"
+
+// 1. create a context to store the name
+const NameContext = createContext();
+
+const D = () => {
+
+  // 3. use the context in the component
+
+  const { name, setName } = useContext(NameContext);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setName('Jane')
+    }, 5000);
+  })
+
+  return <h1>Hello, {name}!</h1>
+}
+
+const C = () => {
+  return <D />
+}
+
+const B = () => {
+  return <C />
+}
+
+
+// 2. wrap the component tree with the context provider
 const App = () => {
-  return <h1>Hello, World!</h1>
+  
+  const [name, setName] = useState('John')
+
+  return (
+    <NameContext.Provider value={{name, setName}}>
+      <B />
+    </NameContext.Provider>
+  )
 }
 
 export default App;
