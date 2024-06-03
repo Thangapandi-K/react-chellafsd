0ec97fee68c418e097733112817795f55c34b025
Thangapandi-K <thangapandi.chella@gmail.com>
2024-06-03 11:10:57 +0530

    useRef hook example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useRef } from "react";
+
 const App = () => {
-  return <h1>Hello, World!</h1>
+
+  // create a reference using useRef hook
+  const inputRef = useRef(null);
+  const inputRef2 = useRef(null);
+
+
+  const handleClick = () => {
+      // focus the input element here
+      //console.log(inputRef.current.value);
+      inputRef2.current.focus();
+
+  }
+
+  return (
+    <>
+      <input 
+        type="text"
+        ref={inputRef} 
+      />
+      <button onClick={handleClick}>Focus Input</button>
+    <br />    
+    <input type="text" ref={inputRef2} />
+    <button onClick={handleClick}>Focus Input 2</button>
+    </>
+  )
 }
 
 export default App;
