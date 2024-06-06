aa79e0038baa4c889547ba288dfe7ef89ee613c8
Thangapandi-K <thangapandi.chella@gmail.com>
2024-06-06 12:30:56 +0530

    todo completion

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 
 
-const TodoItem = ({todo, OnUpdateTodo}) => {
+const TodoItem = ({todo, OnUpdateTodo, onDeleteTodo}) => {
 
     const handleCheckBoxChange = async () => {
         try {
@@ -26,6 +28,19 @@ const TodoItem = ({todo, OnUpdateTodo}) => {
         }
     }
 
+    const handleDeleteTodo = async () => {
+        try {
+            const response = await axios.delete(
+                `https://665eb2be1e9017dc16f0f570.mockapi.io/todos/${todo.id}`
+            );
+
+            onDeleteTodo && onDeleteTodo(todo.id);
+
+        } catch (error) {
+            console.error(`Failed to delete todo: ${error}`);
+        }
+    }
+
   return (
     <div>
         <input
@@ -35,7 +50,16 @@ const TodoItem = ({todo, OnUpdateTodo}) => {
         />
         <span>
             <strong>{todo.title.toUpperCase()}</strong> : <em>{todo.description}</em>
+        </span> &nbsp;&nbsp;
+        <span>
+            <FontAwesomeIcon
+                icon={faTrash}
+                color="red"
+                fontSize={16}
+                onClick={handleDeleteTodo}
+            />
         </span>
+        <br /> <br />
     </div>
   )
 }
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import TodoItem from '../components/TodoItem';
 
+
 // define a loader function
 export const loader = async() => {
     // make an async call to fetch data
@@ -19,10 +20,42 @@ const Todos = () => {
         // use the data using the useLoader hook
         const { todos: initialTodos } = useLoaderData();
         const[todos, setTodos] = useState(initialTodos);
+        const [title, setTitle] = useState('');
+        const[description, setDescription] = useState('');
+        const [completed, setCompleted] = useState(false);
 
         const handleUpdateTodo = (updatedTodo) => {
             setTodos(todos.map(todo => (todo.id === updatedTodo.id ? updatedTodo: todo)))
         }
+
+        const handleCreateTodo = async (e) => {
+            e.preventDefault();
+
+            // create a new todo object
+            const newTodo = {
+                title,
+                description,
+                completed
+            }
+            // api call to create new todo
+            const response = await axios.post(`https://665eb2be1e9017dc16f0f570.mockapi.io/todos/`, newTodo
+            );
+
+            if (response) {
+                setTitle('');
+                setDescription('');
+                setCompleted(false);
+
+            // update the state
+            response.data && setTodos([...todos, response.data])
+
+            alert("New Todo Created Successfully");
+        }
+    }
+    
+    const handleDeleteTodo = async (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
 
         console.log(todos);
   return (
@@ -37,6 +70,32 @@ const Todos = () => {
                     />
                 ))
             }
+            <form onSubmit={handleCreateTodo}>
+                <input 
+                    type="text"
+                    placeholder='Todo Title...'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <input 
+                    type="text"
+                    placeholder='Todo Description'
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <select
+                    value={completed}
+                    onChange={(e) => setCompleted(e.target.value)}
+                >
+                    <option
+                        value={false}
+                    >Not Completed</option>
+                    <option
+                        value={true}
+                    >Completed</option>
+                </select>
+                <button type="submit">Add Todo</button>
+            </form>
     </div>
   )
 }
